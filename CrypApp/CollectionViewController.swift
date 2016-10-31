09609e329156ac5cdffac62a7b51c5943664a0b5
Ondrej Andrysek <ondrejandrysek@ondrejs-macbook-pro.local>
2016-10-31 18:45:50 +0100

    test

---
+++ CrypApp/CollectionViewController.swift
@@ -0,0 +1,47 @@
+//
+//  CollectionViewController.swift
+//  CrypApp
+//
+//  Created by Ondrej Andrysek on 06/10/2016.
+//  Copyright © 2016 Ondrej Andrysek. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class CollectionViewController: UICollectionViewController {
+
+    var arrayMethods = ["Caesar", "Atbash","Vigenèr"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return arrayMethods.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+    
+        let cellLabel = cell.viewWithTag(1) as! UILabel
+        cellLabel.text = arrayMethods[indexPath.row]
+    
+        return cell
+    }
+    
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        let vcName = arrayMethods[indexPath.row]
+        let viewController = storyboard?.instantiateViewControllerWithIdentifier(vcName)
+        self.navigationController?.pushViewController(viewController!, animated: true)
+    }
+
+}
